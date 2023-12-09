650152fb1003a368f6cd438b33d89731dcb066fd
Zak Cook <zakcook4@gmail.com>
2023-12-09 15:28:30 +0100

    Add solution for day 9 part 2

--- day9/erlang/tut1.erl
+++ day9/erlang/tut1.erl
@@ -2,29 +2,34 @@
 -export([main/0]).
 
 parse_line(Line) ->
-  lists:map(fun(I) -> erlang:list_to_integer(I) end, string:split(Line, " ", all)).
+  lists:map(fun(I) -> erlang:list_to_integer(I) end, string:tokens(Line, " \n")).
 
 parse_stdin() ->
   case io:get_line("") of
     eof -> [];
-    Line -> [parse_line(string:trim(Line)) | parse_stdin()]
+    Line -> [parse_line(Line) | parse_stdin()]
   end.
 
 expand(TestCase) ->
   [H | _] = TestCase,
-  AllZeros = lists:all(fun(I) -> I == 0 end, H),
+  [_ | Tail] = H,
+  Differences = lists:map(fun({I1, I2}) -> I2 - I1 end, lists:zip(H, Tail, trim)),
+  AllZeros = lists:all(fun(I) -> I == 0 end, Differences),
   if AllZeros ->
        TestCase;
     true ->
-       [_ | Tail] = H,
-       expand([lists:map(fun({I1, I2}) -> I2 - I1 end, lists:zip(H, Tail, trim)) | TestCase])
+       expand([Differences | TestCase])
   end.
 
-solve_testcase(TestCase) ->
-  ListOfLists = expand([TestCase]),
-  lists:foldr(fun(L, Accu) -> Accu + lists:last(L) end, 0, ListOfLists).
+solve_part1_testcase(ExpandedTestCase) ->
+  lists:foldl(fun(L, Accu) -> Accu + lists:last(L) end, 0, ExpandedTestCase).
 
+solve_part2_testcase(ExpandedTestCase) ->
+  lists:foldl(fun([H | _], Accu) -> H - Accu end, 0, ExpandedTestCase).
 
 main() ->
-  SolvedTestCases = lists:map(fun(T) -> solve_testcase(T) end, parse_stdin()),
-  erlang:display(lists:sum(SolvedTestCases)).
+  ExpandedTestCases = lists:map(fun(T) -> expand([T]) end, parse_stdin()),
+  % solve part 1
+  erlang:display(lists:foldl(fun(TC, Accu) -> Accu + solve_part1_testcase(TC) end, 0, ExpandedTestCases)),
+  % solve part 2
+  erlang:display(lists:foldl(fun(TC, Accu) -> Accu + solve_part2_testcase(TC) end, 0, ExpandedTestCases)).
